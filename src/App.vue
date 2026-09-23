c39f97949b9389d1a459c97de065b576978933f7
sealencehe <sealencehe4u@gmail.com>
2026-09-23 10:25:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CesiumViewer from './components/CesiumViewer.vue'
+</script>
+
+<template>
+  <cesium-viewer></cesium-viewer>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/CesiumViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="map-container"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+// Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ION_TOKEN
+onMounted(() => {
+  const viewer = new Cesium.Viewer('map-container')
+})
+</script>
+
+<style scoped>
+#map-container {
+  width: 100lvw;
+  height: 100vh;
+}
+</style>
